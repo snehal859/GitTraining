0eb903217d042cabd18a4ac829a8e25dc3a7266f
snehal859 <53069556+snehal859@users.noreply.github.com>
2019-07-19 12:22:57 +0530

    Update SuperMath.cs
    Github Change

--- SuperMath.cs
+++ SuperMath.cs
@@ -2,11 +2,11 @@ public class SuperMath
 {
  public int add (int x,int y) 
  {
- return x+y;
+ return x+y+5;
  }
   public int Product (int x,int y) 
  {
- return x*y;
+ return x*y+5;
  }
   public int Sub (int x,int y) 
  {
